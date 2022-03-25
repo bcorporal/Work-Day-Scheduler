0be523d2b831896632a3ac3c5b1eda6dc1d4e789
bcorporal <bcorporal@gmail.com>
2022-03-24 23:51:33 -0400

    layout is in place everything works working on local storage

--- README.md
+++ README.md
@@ -2,36 +2,16 @@
 
 ## Description
 
-AS AN employee with a busy schedule
-I WANT to add important events to a daily planner
-SO THAT I can manage my time effectively
+A scheduler that adds important events to a daily planner
+so you can manage your time effectively between the hours of 9am-5pm.
 
-GIVEN I am using a daily planner to create a schedule
-WHEN I open the planner
-THEN the current day is displayed at the top of the calendar
-WHEN I scroll down
-THEN I am presented with timeblocks for standard business hours
-WHEN I view the timeblocks for that day
-THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-WHEN I click into a timeblock
-THEN I can enter an event
-WHEN I click the save button for that timeblock
-THEN the text for that event is saved in local storage
-WHEN I refresh the page
-THEN the saved events persist
+Each timeblock is color coded to indicate whether it is in the past, present, or future
 
-
-## Installation
-
-What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
 
 ## Usage
 
+![Work Day Scheduler](./assets/Screen%20Shot%202022-03-24%20at%2011.31.55%20PM.png)
 
-
-    ```md
-    ![alt text](assets/images/screenshot.png)
-    ```
 
 ## Features
 
--- script.js
+++ script.js
@@ -1,3 +1,42 @@
-$("#currentDay").text(today.format('MMMM Do YYYY, h:mm:ss a'));
+// Clock in realtime at top of page
+var today = moment();
+$("#currentDay").text(today.format("dddd, MMMM Do YYYY, hh:mm:ss a"));
+window.setInterval(function () {
+$('#currentDay').html(moment().format('ddd MM/DD/y hh:mm:ss'))
+}, 1000);
+// button event to local storage
+$(document).ready(function () {
+	$(".saveBtn").on("click", function () {
+	var text = $(this).prev(".textarea").val();
+	var time = $(this).prev().attr("id");
+	localStorage.setItem(time, JSON.stringify(text));
+	});
+// timeblock color properties
+	var currentHour = new Date().getHours();
+	$(".blocks").each(function () {
+		var val = parseInt($(this).prop("id"));
+		if (val > currentHour) {
+			$(this).addClass("future");
+		} else if (val < currentHour) {
+			$(this).addClass("past");
+		} else if (val == currentHour) {
+			$(this).addClass("present");
+		}
+	});
+});
+// local storage
+//  var saveButton = $(".saveBtn");
+// saveButton.on("click", textarea);
+
+// $function(".saveBtn").on("click", function() {
+// 	var schedule = $(this).attr("id");
+// 	$(this).text(localStorage.getItem(schedule))
+// });
+
+
+//  var saveButton = document.querySelector("#saveBtn");
+//  saveButton.addEventListener("click", function(event) {
+//  	event.preventDefault();
+
 
 
